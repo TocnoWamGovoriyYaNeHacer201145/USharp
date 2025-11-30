8900a4c666c07b9441db525d2784abf90fea0e87
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2025-11-30 23:00:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import time
 import sys
 import subprocess
 import re
+import py_compile
+import os
 
 symbols=['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z','0','1','2','3','4','5','6','7','8','9']
+compiled_filename = None
 
 # Command dictionary
 command_dict = {
@@ -28,21 +31,29 @@ command_dict = {
     "--": "-= 1"
 }
 
-def command_parser(line):
-    mod_line = line
+def command_parser(content):
+    mod_content = content
     for command in command_dict:
         if command and (command[0].isalnum() or command[0] == '_'):
             pattern = r'\b' + re.escape(command) + r'\b'
         else: pattern = re.escape(command) 
-        mod_line = re.sub(pattern, command_dict[command], mod_line)
+        mod_content = re.sub(pattern, command_dict[command], mod_content)
+    open(temp_filename, 'w').write(mod_content)
     try:
-        exec(mod_line)
+        py_compile.compile(temp_filename, cfile=compiled_filename)
+        try:
+            os.remove(temp_filename)
+        except:
+            print(f'Failed to delete temp file {temp_filename}')
     except Exception as e:
-        print(f'Failed to execute file: {e}')
+        print(f'Failed to compile file: {e}')
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2: 
+    if len(sys.argv) != 2:
         print("Usage: <interpreter name> <your .usharp file path>")
         sys.exit(1)
+    file_template = sys.argv[1].replace('.usharp', '')
+    temp_filename = file_template + '.py'
+    compiled_filename = file_template + '.usc'
     with open(sys.argv[1], 'r') as file:
         command_parser(file.read())
